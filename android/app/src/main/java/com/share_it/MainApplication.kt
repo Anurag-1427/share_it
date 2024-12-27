8ca40bb8cabe2ea32bcc5ba4d1e022b539a6ef3a
Anurag Sharma <kaushikanurag2001@gmail.com>
2024-12-27 22:20:03 +0530

    feat: dependency added
    react native tcp socket added

--- android/app/src/main/java/com/share_it/MainApplication.kt
+++ android/app/src/main/java/com/share_it/MainApplication.kt
@@ -12,6 +12,7 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 import com.zoontek.rnpermissions.RNPermissionsPackage
+import com.asterinet.react.tcpsocket.TcpSocketPackage;
 
 class MainApplication : Application(), ReactApplication {
 
@@ -22,6 +23,7 @@ class MainApplication : Application(), ReactApplication {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
               add(RNPermissionsPackage())
+              add(TcpSocketPackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
